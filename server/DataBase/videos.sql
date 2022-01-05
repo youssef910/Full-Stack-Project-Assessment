53c0e59112859760b3104f1a2ad8e9dc51dbd8ed
youssef910 <youssef.sadek910@gmail.com>
2022-01-05 01:02:27 +0000

    update the database scema

--- server/DataBase/videos.sql
+++ server/DataBase/videos.sql
@@ -1,9 +1,10 @@
 drop table if exists videos;
 CREATE TABLE videos (
   id       SERIAL PRIMARY KEY,
-  title     VARCHAR(50) NOT NULL,
+  title     VARCHAR(120) NOT NULL,
   url    VARCHAR(120) NOT NULL,
-  rating  INT
+  rating  INT DEFAULT 0,
+  added_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
 INSERT INTO videos(title ,url,rating)VALUES ('The Coding Train','https://www.youtube.com/watch?v=HerCR8bw_GE',230);
 INSERT INTO videos(title ,url,rating)VALUES ('Mac & Cheese | Basics with Babish','https://www.youtube.com/watch?v=FUeyrEN14Rk',2111);
