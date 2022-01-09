2e1ab602b0766130c2fdbfc7fa0f5f2049b4dc64
youssef910 <youssef.sadek910@gmail.com>
2022-01-09 13:00:44 +0000

    change the background of the card and width

--- client/src/Components/VideoCard.js
+++ client/src/Components/VideoCard.js
@@ -11,9 +11,10 @@ const VideoCard = (props) => {
   const downVote = () => {
     handleDownVote(video.id);
   };
+
   return (
-    <Card style={{ width: '30%' }} className='m-2'>
-      <Card.Title>{video.title}</Card.Title>
+    <Card text='center' bg='info' style={{ width: '31%' }} className='m-2 p-0'>
+      <Card.Header center>{video.title}</Card.Header>
       <Card.Body>
         <ReactPlayer url={video.url} light width={'100%'} controls={true} />
       </Card.Body>
@@ -21,9 +22,11 @@ const VideoCard = (props) => {
       <Card.Body>
         <Row>
           <Col>
-            <Button type='button' variant='primary' disabled>
-              Rating
-              <Badge bg='secondary'>{video.rating}</Badge>
+            <Button type='button' variant='primary'>
+              Rating{' '}
+              <Badge text='danger' pill bg='dark'>
+                {video.rating}
+              </Badge>
             </Button>
           </Col>
           <Col>
@@ -41,34 +44,6 @@ const VideoCard = (props) => {
         </Row>
       </Card.Body>
     </Card>
-    // <div
-    //   className=' card p-0 m-2 card text-dark bg- mb-3 border-primary
-    //          text-center col-sm-12 col-xl-4 col-xxl-4 col-lg-4 col-xsm-12 col-md-4'
-    // >
-    //   <h5 className='card-header '>{video.title}</h5>
-    //   <div className='card-body'>
-    //
-    //   </div>
-    //   <div className='card-footer'>
-    //     <button type='button' className='btn btn-primary disabled'>
-    //       Rating <span class='badge bg-secondary'>{video.rating}</span>
-    //     </button>
-    //     <i
-    //       className='bi bi-hand-thumbs-up '
-    //       style={{ fontSize: '1.5rem', color: 'cornFlowerBlue' }}
-    //       onClick={UpVote}
-    //     >
-    //       up vote
-    //     </i>
-    //     <i
-    //       className='bi bi-hand-thumbs-down text-danger'
-    //       style={{ fontSize: '1.5rem' }}
-    //       onClick={downVote}
-    //     >
-    //       Down vote
-    //     </i>
-    //   </div>
-    // </div>
   );
 };
 
