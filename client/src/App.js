a23ea75e49478d322367a277af17533688ac46e6
youssef910 <youssef.sadek910@gmail.com>
2022-01-09 11:32:12 +0000

    Remove home component

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,15 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import { Routes, Route } from 'react-router-dom';
 import VideoList from './Components/VideoList';
-import Home from './Components/Home';
 import AddVideoForm from './Components/AddVideoForm';
 
 function App() {
   return (
-    <div className='container-fluid  content-align-center '>
+    <div>
       <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
+      <Routes >
+        <Route path='/' element={<VideoList />} />
         <Route path='/Add-Video' element={<AddVideoForm />} />
-        <Route path='/recommended-videos' element={<VideoList />} />
       </Routes>
     </div>
   );
