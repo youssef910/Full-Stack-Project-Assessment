07809dbf8a57576192cb12c77e8ca094efee47c0
youssef sadek <47015754+youssef910@users.noreply.github.com>
2022-01-09 17:56:08 +0000

    Add delete confermation modal and function

--- client/src/Components/VideoList.js
+++ client/src/Components/VideoList.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Container } from 'react-bootstrap';
+import { Row, Container, Modal, Button } from 'react-bootstrap';
 import {
-  downVote,
   getAllVideos,
-  upVote,
   sort,
   search,
+  deleteVideo,
 } from '../api/VideoListManagement';
 import SearchVideos from './SearchVideos';
 import SortDropDown from './SortDropDown';
@@ -13,20 +12,17 @@ import VideoCard from './VideoCard';
 
 const VideoList = () => {
   const [videoList, setVideoList] = useState([]);
+  const [show, setShow] = useState(false);
+  const [deleteId, setDeleteId] = useState();
 
   const pageReload = () => getAllVideos().then((res) => setVideoList(res.data));
 
+  const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
   useEffect(() => {
     pageReload();
   }, [setVideoList]);
-  const handleUpVote = (id) => {
-    upVote(id);
-    pageReload();
-  };
-  const handleDownVote = (id) => {
-    downVote(id);
-    pageReload();
-  };
+
   const handleSorting = (sorter) => {
     sort(sorter).then((data) => setVideoList(data.data));
   };
@@ -34,32 +30,62 @@ const VideoList = () => {
     search(searchWord).then((data) => setVideoList(data.data));
   };
 
+  const AskConfirmDelete = (id) => {
+    setShow(true);
+    setDeleteId(id);
+  };
+  const handleConfirm = () => {
+    setShow(false);
+    deleteVideo(deleteId);
+    window.location.reload();
+  };
   return (
-    <Container
-      fluid
-      className='justify-content-center'
-      style={{ backgroundColor: 'black' }}
-    >
-      <Row className='justify-content-center'>
-        <header className='col App-header  text-center text-light'>
-          <h1>Video Recommendation</h1>
-        </header>
-      </Row>
-      <Row className=' my-3 justify-content-center'>
-        <SortDropDown handleSorting={handleSorting} />
-        <SearchVideos handleSearch={handleSearch} />
-      </Row>
-      <Row className=' justify-content-around'>
-        {videoList.map((video, key) => (
-          <VideoCard
-            video={video}
-            key={key}
-            handleDownVote={handleDownVote}
-            handleUpVote={handleUpVote}
-          />
-        ))}
-      </Row>
-    </Container>
+    <div>
+      {!show ? (
+        <Container
+          fluid
+          className='justify-content-center'
+          style={{ backgroundColor: 'black' }}
+        >
+          <Row className='justify-content-center'>
+            <header className='col App-header  text-center text-light'>
+              <h1>Video Recommendation</h1>
+            </header>
+          </Row>
+          <Row className=' my-3 justify-content-center'>
+            <SortDropDown handleSorting={handleSorting} />
+            <SearchVideos handleSearch={handleSearch} />
+          </Row>
+          <Row className=' justify-content-around'>
+            {videoList.map((video, key) => (
+              <VideoCard
+                video={video}
+                key={key}
+                pageReload={pageReload}
+                confirmDeleteVideo={AskConfirmDelete}
+              />
+            ))}
+          </Row>
+        </Container>
+      ) : (
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={{ color: 'red' }}>
+            Are you sure you want to delete this video
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='primary' onClick={handleConfirm}>
+              yes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
   );
 };
 
