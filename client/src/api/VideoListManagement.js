8110cbaf7acccf4849a1b24c3c758229e3fde340
youssef sadek <47015754+youssef910@users.noreply.github.com>
2022-01-09 22:45:09 +0000

    Mode AddVideo Func to FE API

--- client/src/api/VideoListManagement.js
+++ client/src/api/VideoListManagement.js
@@ -26,3 +26,12 @@ export const deleteVideo = (id) =>
   fetch(`http://localhost:5000/api/delete/${id}`, {
     method: 'DELETE',
   }).then((res) => res.json());
+
+export const addVideo = (newVideoData) =>
+  fetch('http://localhost:5000/api/add-video', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newVideoData),
+  });
